aba6c488e3c47ef661123f32ef978e959a84faba
UmarShahid361 <umar.shahid87@gmail.com>
2025-12-02 14:53:37 +0500

    Basics covered

---
+++ exercise.py
@@ -0,0 +1,7 @@
+even = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        even += 1
+        print(number)
+
+print(f"We have {even} numbers")
--- loops.py
+++ loops.py
@@ -1,3 +1,9 @@
 # FOR LOOP
 for number in range(3):
     print("Testing", number)
+
+# WHILE LOOP
+number = 100
+while number > 0:
+    print(number)
+    number //= 2
